cf439a2c249af73612b388c3a4c51bb6b633e380
Jni <jni.viens@gmail.com>
2016-03-17 23:18:36 -0400

    Initial setup

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# How to play
+
+Open ReservationServiceITest and start uncommenting tests!
+
+Run them with phpunit ReservationServiceITest.php
---
+++ ReservationServiceITest.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('ReservationService.php');
+
+class ReservationServiceITest extends PHPUnit_Framework_TestCase {
+
+  public function test_asdf() {
+    $this->assertTrue(false);
+  }
+}
